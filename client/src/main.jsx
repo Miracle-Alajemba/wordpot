8798ef9b1c4336514432e36b8daa1a5a35407538
miraclealajemba <miraclealajemba@gmail.com>
2026-05-10 02:49:33 +0100

    added some update for analysis

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ReownProvider } from "./providers/reown-provider.jsx";
 import "./styles.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ReownProvider>
