aab0d0c9dbd4dd073c6aaf8f60e397cabfbc10b1
miraclealajemba <miraclealajemba@gmail.com>
2026-07-28 10:09:43 +0100

    feat(client): add game instructions and rules content registry

---
+++ client/src/utils/game-rules.js
@@ -0,0 +1,5 @@
+export const GAME_RULES = [
+  "Form words using the available letter tiles.",
+  "Longer words earn higher point multipliers.",
+  "Play against the clock to reach the top of the leaderboard."
+];
