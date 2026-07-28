6ad7dda7fece1390c4e6801e03384d219e8d4274
miraclealajemba <miraclealajemba@gmail.com>
2026-07-28 10:09:53 +0100

    fix(client): update sound preloader test to native assert.doesNotThrow

--- client/src/utils/sound-preloader.test.js
+++ client/src/utils/sound-preloader.test.js
@@ -3,5 +3,5 @@ import assert from "node:assert";
 import { preloadAudio } from "./sound-preloader.js";
 
 test("safely executes sound preloader", () => {
-  expect(() => preloadAudio("click.mp3")).not.toThrow();
+  assert.doesNotThrow(() => preloadAudio("click.mp3"));
 });
