80cdd8b0c646c0fd58d6bcb5591dca200b096a42
miraclealajemba <miraclealajemba@gmail.com>
2026-07-28 10:10:26 +0100

    feat(server): add Celo Mainnet blockchain configuration metadata

---
+++ server/src/constants/network-config.js
@@ -0,0 +1,7 @@
+export const CELO_MAINNET_CONFIG = {
+  chainId: 42220,
+  chainName: "Celo Mainnet",
+  rpcUrl: "https://forno.celo.org",
+  blockExplorer: "https://celoscan.io",
+  symbol: "CELO",
+};
