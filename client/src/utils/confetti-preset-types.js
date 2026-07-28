885f317ce56735959cba0878a0cb68ea0d1523ae
miraclealajemba <miraclealajemba@gmail.com>
2026-07-28 10:14:17 +0100

    feat(client): add confetti particle effect preset configurations

---
+++ client/src/utils/confetti-preset-types.js
@@ -0,0 +1,5 @@
+export const CONFETTI_PRESETS = {
+  VICTORY: { particleCount: 120, spread: 90, startVelocity: 45 },
+  LEVEL_UP: { particleCount: 60, spread: 60, startVelocity: 30 },
+  REWARD: { particleCount: 40, spread: 50, startVelocity: 25 },
+};
