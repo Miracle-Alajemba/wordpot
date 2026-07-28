2092eb620952d3dd26f3420da2ec338c9865dc72
miraclealajemba <miraclealajemba@gmail.com>
2026-07-28 10:14:16 +0100

    feat(client): add browser fullscreen mode toggle helper

---
+++ client/src/utils/fullscreen-helper.js
@@ -0,0 +1,8 @@
+export function toggleFullScreen() {
+  if (typeof document === "undefined") return;
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen?.().catch(() => {});
+  } else {
+    document.exitFullscreen?.().catch(() => {});
+  }
+}
