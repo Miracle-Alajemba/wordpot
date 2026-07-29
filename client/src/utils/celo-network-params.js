8305ab234b9bd1a056c17ac538849c070e02e59b
miraclealajemba <miraclealajemba@gmail.com>
2026-07-29 17:56:52 +0100

    feat(client): add wallet wallet_addEthereumChain parameters for Celo Mainnet

---
+++ client/src/utils/celo-network-params.js
@@ -0,0 +1,7 @@
+export const CELO_MAINNET_PARAMS = {
+  chainId: "0xa4ec",
+  chainName: "Celo Mainnet",
+  nativeCurrency: { name: "CELO", symbol: "CELO", decimals: 18 },
+  rpcUrls: ["https://forno.celo.org"],
+  blockExplorerUrls: ["https://celoscan.io"],
+};
